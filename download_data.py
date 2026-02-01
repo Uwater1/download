48ed82c41dbf44a1d8c4f7b8a25349350d4cfd68
Uwater1 <194621470+Uwater1@users.noreply.github.com>
2026-02-01 02:23:33 -0500

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import os
 from datetime import datetime
 import pandas as pd
+import pandas_ta as ta
 import time
 
 TICKERS = {
@@ -27,7 +28,7 @@ TICKERS = {
 }
 
 current_date = datetime.now().strftime("%Y-%m-%d")
-folder_path = current_date
+folder_path = current_date + "_5m"
 
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
